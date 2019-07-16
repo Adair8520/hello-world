24cdff73d196bd4354e37e52484331a6fe1ebf84
Adair8520 <254069610@qq.com>
2019-07-16 20:46:34 +0800

    lodash

--- lodash/adair8520-lodash.js
+++ lodash/adair8520-lodash.js
@@ -3,3 +3,15 @@ var USERNAME = {
     return ary.filter(it => it)
   },
 }
+
+function flatten(ary) {
+  var result = []
+  for(var item of ary) {
+    if (Array.isArray(item)) {
+      result.push(...item)
+    } else {
+      result.push(item)
+    }
+  }
+  return result
+}
